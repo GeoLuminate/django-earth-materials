4a70648151f511f8ca73e17e5fc709be902af3a5
Sam <samuel.scott.jennings@gmail.com>
2022-08-16 18:21:48 +0200

    add ability to access data in csv

--- earth_materials/utils.py
+++ earth_materials/utils.py
@@ -1,6 +1,7 @@
 from django.db.models import Q, F
 from django.db.models.functions import Substr
 from earth_materials.models import EarthMaterial
+import os
 
 ParentPath = Substr('path', 1, (F('depth') - 1) * EarthMaterial.steplen )
 RootPath = Substr('path', 1, EarthMaterial.steplen)
@@ -27,3 +28,8 @@ def include_ancestors(queryset=None):
     for node in queryset:
         queryset |= node.get_ancestors()
     return queryset.values_list('id')
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+database = os.path.join(dir_path,'data/data.csv')
